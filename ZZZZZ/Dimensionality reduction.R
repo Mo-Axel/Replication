cee7e63e751965f455f86d5a82deaaef1e5d4713
Mo-Axel <76988118+Mo-Axel@users.noreply.github.com>
2022-07-21 16:55:13 +1000

    ad

--- ZZZZZ/Dimensionality reduction.R
+++ ZZZZZ/Dimensionality reduction.R
@@ -25,13 +25,16 @@ scf[year == 2019,107:232]
 scf = read.table("all years with timevar.txt",sep=",",head = T)
   
 scf[,107:232]=lapply(scf[,107:232],fac_num)
-scf = scf[,-1]
+scf2 = scf2[,-1]
 scf = one_hot(as.data.table(scf))
 scf = as.data.frame(scf)
 
-randomForest_rf<-randomForest(c_Lli_ty~.,data = scf,
-                              ntree = 200,
-                              mtry = 20,
+ind<-sample(2,nrow(scf2),replace = TRUE,prob = c(0.8,0.2))
+traindata<-scf2[ind == 1,]
+testdata<-scf2[ind == 2,]
+randomForest_rf2<-randomForest(c_Lli_ty~.,data = traindata,
+                              ntree = 130,
+                              mtry = 30,
                               proximity = TRUE,
                               importance = TRUE)
 A = importance(randomForest_rf,type = 1)
@@ -40,3 +43,6 @@ B=predict(randomForest_rf, testdata, type="response",
         norm.votes=TRUE, predict.all=FALSE, proximity=FALSE, nodes=FALSE)
 hist(B,xlab = "Weight",col = "yellow",border = "blue")
 hist(testdata[,117],add = T,col = "green")
+write.csv(A, file = "ranking2.csv")
+
+
