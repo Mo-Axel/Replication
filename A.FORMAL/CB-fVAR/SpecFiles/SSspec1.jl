60c36aaf0d0168b8ba8f2f9d83e523082199e8f9
Mo-Axel <76988118+Mo-Axel@users.noreply.github.com>
2022-08-29 00:05:45 +1000

    cj

---
+++ A.FORMAL/CB-fVAR/SpecFiles/SSspec1.jl
@@ -0,0 +1,16 @@
+SampleStart = 1989.25
+SampleEnd   = 2017.5
+
+nlags      = 1
+Tdrop      = 3    # initial observations to be dropped and used for lag initialization, etc
+
+# model configuration
+K         = 10
+lambda1   = 5.3
+lambda2   = 403
+lambda3   = 95
+
+# priors
+iwishdf = 5
+
+silent = 0
